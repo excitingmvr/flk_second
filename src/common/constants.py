6eca8e351c1b303fbebc1ae550eb013a2e7422b1
Tony <excitingmvr@gmail.com>
2020-11-09 15:16:11 +0900

    third

--- src/common/constants.py
+++ src/common/constants.py
@@ -11,13 +11,13 @@ class Constants():
     #hashing (must 4 char)
     HASH_SUFFIX = 'KoNa'
 
-    PROJECT_PATH_WINDOWS = 'D:/factory/workspace_flask/flk_second_2'
+    PROJECT_PATH_WINDOWS = 'D:/factory/workspace_flask/flk_second'
     PROJECT_PATH_LINUX = ''
     PROJECT_PATH_MAC = ''
 
-    SOURCE_PAHT_WINDOWS = 'D:/factory/workspace_flask/flk_second_2/src'
+    SOURCE_PAHT_WINDOWS = 'D:/factory/workspace_flask/flk_second/src'
     SOURCE_PAHT_LINUX = ''
     SOURCE_PAHT_MAC = ''
     
-    UPLOAD_FOLDER_FULL = 'D:/factory/workspace_flask/flk_second_2/static/upload'
+    UPLOAD_FOLDER_FULL = 'D:/factory/workspace_flask/flk_second/static/upload'
     UPLOAD_FOLDER_FOR_LINK = '/static/upload'
--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@
 
 import sys
 
-sys.path.insert(0, 'D:/factory/workspace_flask/flk_second_2')
+sys.path.insert(0, 'D:/factory/workspace_flask/flk_second')
 
 from app import app as application
